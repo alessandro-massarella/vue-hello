31e67179ac81469f4835624e22c0ae5c25b55548
alessandro <alessandro.massarella@gmail.com>
2020-11-16 15:56:50 +0100

    Msg + Img

---
+++ main.js
@@ -0,0 +1,11 @@
+// Utilizzando il framework Vue
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere un’immagine presa anch’essa da un data.
+
+const myApp = new Vue ({
+  el: ('#root'),
+  data: {
+    message: 'Eliska Junkova',
+    image: 'https://www.ilpost.it/wp-content/uploads/2020/11/eliska-junkova.png'
+  }
+});
